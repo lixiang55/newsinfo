6de6ece4e47dffcec0f16b3ee294450c57d39433
lixiang55 <47545875+lixiang55@users.noreply.github.com>
2019-02-12 11:04:56 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ from flask import Flask
 from config import dict_config
 app = Flask(__name__)
 app.config.from_pyfile(dict_config['config'])
-
+555
 if __name__ == '__main__':
     app.run()
-    5546541565456466
+  
